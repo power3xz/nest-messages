f7ee09fac7ea801f7baa26989b139b6b3c1df67f
devj <power3xz@gmail.com>
2022-05-22 17:10:05 +0900

    데코레이터 추가

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -6,8 +6,8 @@ export class MessagesController {
   listMessages() {}
 
   @Post()
-  createMessages() {}
+  createMessages(@Body() body: any) {}
 
   @Get('/:id')
-  getMessage() {}
+  getMessage(@Param('id') id: string) {}
 }
