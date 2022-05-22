ad9f2b6ee64ba575f7f4a6633886bc3f67b62a24
devj <power3xz@gmail.com>
2022-05-22 20:32:04 +0900

    객체생성 -> DI 코드로 변경

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
+import { MessagesService } from './messages.service';
+import { MessagesRepository } from './messages.repository';
 
 @Module({
+  providers: [MessagesService, MessagesRepository],
   controllers: [MessagesController],
 })
 export class MessagesModule {}
